08564b16c732a16e0d944d820ec1a8cd01e95d4a
CalmDeveloper <demir445@gmail.com>
2022-02-01 16:07:56 +0300

    mkn

---
+++ JS/lesson 1/homework/main.js
@@ -0,0 +1,130 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let a = `hello`;
+// let b = `owu`;
+// let c = `com`;
+// let d = `ua`;
+// let one = 1;
+// let two = 10;
+// let text = -999;
+// let hello = 123;
+// let bottom = 3.14;
+// let doc = 2.7;
+// let body = 16;
+// let head = true;
+// let color = false;
+
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(one);
+// console.log(two);
+// console.log(text);
+// console.log(hello);
+// console.log(bottom);
+// console.log(doc);
+// console.log(body);
+// console.log(head);
+// console.log(color);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(one);
+// alert(two);
+// alert(text);
+// alert(hello);
+// alert(bottom);
+// alert(doc);
+// alert(body);
+// alert(head);
+
+
+
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(one);
+// document.write(two);
+// document.write(text);
+// document.write(hello);
+// document.write(bottom);
+// document.write(doc);
+// document.write(body);
+// document.write(head);
+//
+// // Переприсвоїти кожній змінній з завдання значення на довільне.
+// a = `khi`;
+// b = `cknk`;
+// c = `kjnjn`;
+// d = `knkj`;
+// one = 7;
+// two = 1494;
+// text = -79;
+// hello = 150;
+// bottom = 5;
+// doc = 4.76;
+// body = 17;
+// head = false;
+// color = true;
+
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(one);
+// console.log(two);
+// console.log(text);
+// console.log(hello);
+// console.log(bottom);
+// console.log(doc);
+// console.log(body);
+// console.log(head);
+// console.log(color);
+
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// alert(one);
+// alert(two);
+// alert(text);
+// alert(hello);
+// alert(bottom);
+// alert(doc);
+// alert(body);
+// alert(head);
+
+
+
+// document.write(a);
+// document.write(b);
+// document.write(c);
+// document.write(d);
+// document.write(one);
+// document.write(two);
+// document.write(text);
+// document.write(hello);
+// document.write(bottom);
+// document.write(doc);
+// document.write(body);
+// document.write(head);
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// firstName = `Taras`;
+// middleName = `Ivanovych`;
+// lastName = `Maksymiv`
+
+// let person = [firstName, middleName, lastName];
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+let name = prompt("Вкажіть своє ім'я");
+let middleName = prompt("Вкажіть ім'я по батькові");
+let age = prompt("Вкажіть свій вік");
+alert("Вітаю"+" "+name+" "+middleName+"."+" "+"Тобі"+" "+age+" "+"років.");
