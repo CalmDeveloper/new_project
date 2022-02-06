4f9b29dd6b1dd5fcc4e8089d132a5187a2d956aa
CalmDeveloper <demir445@gmail.com>
2022-02-06 15:55:36 +0300

    lesson 3 hw +classwork 1/2

---
+++ JS/lesson 3/classwork/main.js
@@ -0,0 +1,107 @@
+// зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+
+let masive = [2,17,13,6,22,31,45,66,100,-18];
+
+//     2. перебрати його циклом for
+// let masive = [2,17,13,6,22,31,45,66,100,-18];
+// // for (i = 0; i < masive.length; i++){
+// //
+// //     console.log(masive[i])
+// //
+// // }
+
+//9.3. reverse
+// for (i = masive.length-1; i >=0; i--){console.log(masive[i])}
+
+
+
+//     3. перебрати циклом while та вивести  числа тільки з непарним індексом
+// i = 0;
+// while ( i < masive.length){
+//    if  (masive[i] % 2 ===1){console.log(masive[i])}
+//     i++;
+// }
+
+//9.3. reverse
+// i = masive.length-1;
+// while (i >=0){
+//     if(masive[i] % 2 ===1){console.log(masive[i]);
+//     }
+//     i--;
+// }
+
+
+
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+// for (i = 0; i < masive.length ; i++){
+//     if ( masive[i] % 2 ===1){console.log(masive[i])}
+// }
+
+//9.4. reverse
+// for (i = masive.length-1; i >=0; i--){
+//     if(masive[i] % 2===1){console.log(masive[i])}
+// }
+
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+// i = 0;
+// while ( i < masive.length){
+//     if  (masive[i] % 2 ===0){console.log(masive[i])}
+//     i++;
+// }
+
+//9.5. reverse
+// i = masive.length-1;
+// while (i >=0){
+//     if(masive[i] % 2===0){console.log(masive[i])}
+//     i--;
+// }
+
+
+
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+// for (i = 0; i < masive.length; i++) {
+//     if(masive[i] % 2 ===0){console.log(masive[i])}
+// }
+
+
+//9.6. reverse
+// for (i = masive.length-1; i>=0; i-- ){if(masive[i] % 2 ===0){
+//     console.log(masive[i]);
+// }
+// }
+
+
+
+
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+// for (i = 0; i < masive.length; i++){
+//     if(masive[i] % 3 ===0){masive[i] = "okten";
+//     console.log(masive[i])}
+//     console.log(masive[i])
+// }
+
+//9.7. reverse
+// for (i = masive.length-1; i >=0; i--){
+//     if(masive[i] % 3 ===0){masive[i] = "okten";
+//     console.log(masive[i])}
+//     console.log(masive[i])
+// }
+
+
+
+
+
+// 8. вивести масив в зворотньому порядку.
+// for (let i = masive.length - 1; i >= 0; i--) {
+//
+//     console.log(masive[i])
+// }
+
+
+
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
