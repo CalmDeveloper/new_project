ae5c14bff28f1606540b91727dac11a699962618
CalmDeveloper <demir445@gmail.com>
2022-02-09 01:06:16 +0300

    swc

--- JS/lesson 4/classwork/main.js
+++ JS/lesson 4/classwork/main.js
@@ -72,24 +72,52 @@
 
 
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
-let kkk = [1,2,3,4,5657,68,79,79];
-function minandmax(array) {
-    let min = array[0];
-    let max = array[0];
-    for (const arrayElement of array) {
-        if (min < arrayElement){min = arrayElement}
-        if (max > arrayElement){max = arrayElement}
+// let kkk = [1,2,3,4,5657,68,79,79];
+// function minandmax(array) {
+//     let min = array[0];
+//     let max = array[0];
+//     for (const arrayElement of array) {
+//         if (max < arrayElement){max = arrayElement}
+//         if (min > arrayElement){min = arrayElement}
+//
+//     }
+//     console.log(max);
+//     return (min)
+//
+//
+//
+// }
+// let a = minandmax(kkk)
 
-    }
-    return min
-    return max
-
-
-}
-let a = minandmax(kkk)
-console.log(a)
 
 // - створити функцію яка заповнює масив рандомними числами
-// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+//     (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+// function pushRandomNum(length) {
+//     let num = [];
+// for (let i= 0; i < length; i++) {
+//     num.push(Math.floor(Math.random()*100))
+// }
+// return num;
+// }
+// let a = pushRandomNum(length)
+// document.write(pushRandomNum(100))
+
+
+
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+//
+// function rand(length, limit ) {
+//
+//     let num = [];
+//     for (let i = 0; i < length; i++){
+//         num.push(Math.round(Math.random() * limit ));
+//     }
+//     return num;
+// }
+
+// document.write(rand(10, 20))
+
+
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+
