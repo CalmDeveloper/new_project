d02c3d0e5ce2f90f2a52107a32fe243ff76a3ceb
CalmDeveloper <demir445@gmail.com>
2022-02-20 13:25:46 +0300

    4 additional+7 hw+cw+add 8hw+cw

--- JS/lesson 8/addational/main.js
+++ JS/lesson 8/addational/main.js
@@ -1,2 +1,21 @@
 
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
+
+
+let box = [];
+let getChildren = document.body.children
+for (let child of getChildren) {
+   if (child.children){
+       for (const childOfChild of child.children) {
+
+          if (childOfChild.children){box.push(childOfChild)}
+       }
+   }
+}
+console.log(box)
+
+
+
+
+
+
