52c56adf1302bd7a80729e8e45a9b6cc43a696d7
CalmDeveloper <demir445@gmail.com>
2022-02-26 01:06:34 +0300

    10hm

--- JS/lesson 10/homework/main.js
+++ JS/lesson 10/homework/main.js
@@ -1,4 +1,4 @@
-// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
+// 1. - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
 // let div = document.createElement(`div`);
 // document.body.append(div)
 // div.id = `text`
@@ -15,7 +15,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//     - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// 2.    - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
 // let button  = document.createElement(`button`);
 // document.body.append(button);
 // button.classList.add(`second_button`);
@@ -29,9 +29,9 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+//3. - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
 
-let button =  document.getElementById(`but`)
+// let button =  document.getElementById(`but`)
 
 
 // button.onclick = function () {
@@ -44,9 +44,50 @@ let button =  document.getElementById(`but`)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// - Создайте меню, которое раскрывается/сворачивается при клике
+// 4.- Создайте меню, которое раскрывается/сворачивается при клике
+// let button = document.querySelector(`h1`)
+// let list = document.getElementsByClassName(`hiden`)[0]
+// button.onclick = function () {
+//     list.classList.toggle(`hiden`)
+// }
 
-//
-// - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
+// 5. - Создать список комментариев , пример объекта коментария - {title : 'lorem', body:'lorem ipsum dolo sit ameti'}.
 //     Вывести список комментариев в документ, каждый в своем блоке.
 //     Добавьте каждому комментарию по кнопке для сворачивания его body.
+
+let comments = [
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+    {title: 'lorem', body: 'lorem ipsum dolo sit ameti'},
+];
+
+let  mainDiv = document.createElement(`div`)
+for (const comment of comments) {
+    let  div = document.createElement(`div`);
+    let  button = document.createElement(`button`);
+    let  br = document.createElement(`br`);
+    let  ul = document.createElement(`ul`);
+    let  liTitle = document.createElement(`li`);
+    let  liBody = document.createElement(`li`);
+
+    liTitle.innerText = comment.title;
+    liBody.innerText = comment.body;
+
+    button.innerText = `закрий мене`;
+    document.body.append(mainDiv)
+    mainDiv.append(div)
+    div.append(button,br,ul);
+    ul.append(liTitle,liBody)
+
+    button.onclick = function () {
+        ul.classList.toggle(`hiden`)
+    }
+
+}
+
+
+
+
+
+
